06f8a56e2b6e80a3efeb5b7e28a1bf36c54399af
SumiAkter2 <akteralisha36@gmail.com>
2023-02-04 15:20:08 +0600

    post data

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const student = {
-  name: "Sumi Akter",
-  id: 8,
-  favSub: "Mathematics",
-};
+app.use(cors());
+app.use(express.json());
+const students = [
+  { name: "Rasheduzzaman", id: 1, favSub: "Biology" },
+  { name: "Fariha Tabassum", id: 2, favSub: "Physics" },
+  { name: "Sumi Akter", id: 3, favSub: "Mathematics" },
+];
+
+app.get("/students", (req, res) => {
+  res.send(students);
+});
+
+app.post("/students", (req, res) => {
+  console.log("post api call");
+  const student = req.body;
+  student.id = students.length + 1;
+  students.push(student);
+  res.send(student);
+  console.log(student);
+});
+
+
+
 
 app.get("/", (req, res) => {
-    const query = req.body;
   res.send("Simple node js connecting 2023");
 });
 
