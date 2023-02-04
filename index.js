3a69132c57b65969bb1b562f8a9792f2aa605a0b
SumiAkter2 <akteralisha36@gmail.com>
2023-02-04 13:57:26 +0600

    added express

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const student = {
+  name: "Sumi Akter",
+  id: 8,
+  favSub: "Mathematics",
+};
+
+app.get("/", (req, res) => {
+    const query = req.body;
+  res.send("Simple node js connecting 2023");
+});
+
+app.listen(port, () => {
+  console.log("Simple node js connecting", port);
+});
