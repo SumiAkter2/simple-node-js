76458c502b7dbbb4341723b825527a40b16d81c9
SumiAkter2 <akteralisha36@gmail.com>
2023-02-05 11:48:59 +0600

    added query

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ async function run() {
 run().catch((err) => {
   console.log(err);
 });
+// app.get('/users', (req, res) => {
+//     if(req.query.name){
+//         const search = req.query.name;
+//         const filtered = users.filter(usr => usr.name.toLowerCase().   indexOf(search) >= 0);
+//         res.send(filtered);
+//     }
+//     else {
+//         res.send(users);
+//     }
+
+// });
 
 // app.post("/students", (req, res) => {
 //   console.log("post api call");
